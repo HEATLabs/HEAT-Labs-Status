6fb71a6522dcb10655f861865356a9ae2e0b6e45
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-10-04 06:23:34 +0300

    removed leftover texts in misc.js file (oops)

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,6 +1,1 @@
 // Miscellaneous Scripts
-
-added new easter egg JS imports in all main site pages
-removed all easter egg scripts from misc.js file
-added dedicated JS file for radio easter egg
-added dedicated JS file for go-to easter egg
